010a86a585a9c64ce6eed35de9215f7ec13603d5
Samuel Ventimiglia <sviluppo@tulain.com>
2016-10-18 18:03:00 +0200

    Initial Commit
    Signed-off-by: Samuel Ventimiglia <sviluppo@tulain.com>

---
+++ src/Config/AuthSetup.php
@@ -0,0 +1,24 @@
+<?php
+namespace Config;
+
+/**
+ *
+ * @author sviluppo
+ *        
+ */
+class AuthSetup
+{
+
+    /**
+     *
+     * @var string
+     */
+    const BASEURI = '';
+
+    /**
+     *
+     * @var string
+     */
+    const TOKEN = null;
+}
+
---
+++ src/Interfaces/FirebaseInterface.php
@@ -0,0 +1,95 @@
+<?php
+namespace Interfaces;
+
+/**
+ * Interface FirebaseInterface
+ *
+ * @author Ventimiglia Samuel
+ *         @sice 18/10/2016
+ *        
+ * @package Firebase
+ */
+interface FirebaseInterface
+{
+
+    /**
+     *
+     * @param
+     *            $token
+     * @return mixed
+     */
+    public function setToken($token);
+
+    /**
+     *
+     * @param
+     *            $baseURI
+     * @return mixed
+     */
+    public function setBaseURI($baseURI);
+
+    /**
+     *
+     * @param
+     *            $seconds
+     * @return mixed
+     */
+    public function setTimeOut($seconds);
+
+    /**
+     *
+     * @param
+     *            $path
+     * @param
+     *            $data
+     * @param
+     *            $options
+     * @return mixed
+     */
+    public function put($path, $data, $options = array());
+
+    /**
+     *
+     * @param
+     *            $path
+     * @param
+     *            $data
+     * @param
+     *            $options
+     * @return mixed
+     */
+    public function post($path, $data, $options = array());
+
+    /**
+     *
+     * @param
+     *            $path
+     * @param
+     *            $data
+     * @param
+     *            $options
+     * @return mixed
+     */
+    public function patch($path, $data, $options = array());
+
+    /**
+     *
+     * @param
+     *            $path
+     * @param
+     *            $options
+     * @return mixed
+     */
+    public function get($path, $options = array());
+
+    /**
+     *
+     * @param
+     *            $path
+     * @param
+     *            $options
+     * @return mixed
+     */
+    public function delete($path, $options = array());
+}
+
