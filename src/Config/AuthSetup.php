2c74a4200746cdbf136070230f47a960e3cf5e2b
Samuel Ventimiglia <sviluppo@tulain.com>
2016-10-19 17:12:18 +0200

    Fixed authentication keys
    Signed-off-by: Samuel Ventimiglia <sviluppo@tulain.com>

--- src/Config/AuthSetup.php
+++ src/Config/AuthSetup.php
@@ -1,5 +1,6 @@
 <?php
-namespace Config;
+declare(strict_types = 1);
+namespace ZendFirebase\Config;
 
 /**
  *
@@ -13,12 +14,61 @@ class AuthSetup
      *
      * @var string
      */
-    const BASEURI = '';
+    private $baseUri;
 
     /**
      *
      * @var string
      */
-    const TOKEN = null;
+    private $serverToken;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     *
+     * @return the $baseUri
+     */
+    public function getBaseUri(): string
+    {
+        return $this->baseUri;
+    }
+
+    /**
+     *
+     * @return the $serverToken
+     */
+    public function getServerToken(): string
+    {
+        return $this->serverToken;
+    }
+
+    /**
+     *
+     * @param string $baseUri            
+     */
+    public function setBaseUri($baseUri)
+    {
+        if (\strlen($baseUri) == 0) {
+            trigger_error('You must provide a baseURI variable.', E_USER_ERROR);
+        }
+        $baseUri .= (substr($baseUri, - 1) == '/' ? '' : '/');
+        
+        $this->baseUri = \trim($baseUri);
+    }
+
+    /**
+     *
+     * @param string $serverToken            
+     */
+    public function setServerToken($serverToken)
+    {
+        if ($serverToken == '' or \strlen($serverToken) == 0) {
+            trigger_error('You must provide a baseURI variable.', E_USER_ERROR);
+        }
+        $this->serverToken = \trim($serverToken);
+    }
 }
 
