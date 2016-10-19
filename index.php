bb356d9504f06f538bff84ed1a09d3fd277d9259
Samuel Ventimiglia <sviluppo@tulain.com>
2016-10-19 17:11:42 +0200

    Add keywords
    Signed-off-by: Samuel Ventimiglia <sviluppo@tulain.com>

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+use ZendFirebase\Firebase\FirebaseInit, ZendFirebase\Config\AuthSetup;
+require_once __DIR__ . '/vendor/autoload.php';
+require 'src/Config/AuthSetup.php';
+require 'src/FirebaseInit.php';
+
+$auth = new AuthSetup();
+
+$auth->setBaseUri('https://breweriesandroid-557fc.firebaseio.com/');
+$auth->setServerToken('BTYdglBnqZHxPDok65sldGZW67n6RNKV2cJzXnJK');
+
+var_dump($auth);
+$firebase = new FirebaseInit($auth);
