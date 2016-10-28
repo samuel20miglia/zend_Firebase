ba7bb3f9ff79a70925fcf187f35f81759f804f41
GhostByte <info@samuelventimiglia.it>
2016-10-29 00:03:44 +0200

    Added name spase in class

--- tests/FirebaseResponceTest.php
+++ tests/FirebaseResponceTest.php
@@ -1,4 +1,6 @@
 <?php
+namespace ZendFirebaseTest;
+
 use ZendFirebase\FirebaseResponce;
 
 require_once 'src/FirebaseResponce.php';
@@ -24,10 +26,10 @@ class FirebaseResponceTest extends PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        
+
         // create instance of FirebaseResponce() object
         $this->firebaseResponce = new FirebaseResponce();
-        
+
         $this->firebaseResponce->setFirebaseData([]);
         $this->firebaseResponce->setOperation('GET');
         $this->firebaseResponce->setStatus(200);
@@ -39,7 +41,7 @@ class FirebaseResponceTest extends PHPUnit_Framework_TestCase
     protected function tearDown()
     {
         $this->firebaseResponce = null;
-        
+
         parent::tearDown();
     }
 
@@ -49,7 +51,7 @@ class FirebaseResponceTest extends PHPUnit_Framework_TestCase
     public function testConstruct()
     {
         $this->firebaseResponce->__construct();
-        
+
         $this->assertNotEquals(FirebaseResponce::class, $this->firebaseResponce);
     }
 
@@ -69,7 +71,7 @@ class FirebaseResponceTest extends PHPUnit_Framework_TestCase
     {
         /* not empty */
         $this->assertNotEmpty($this->firebaseResponce->getOperation());
-        
+
         /* type string */
         $this->assertInternalType('string', $this->firebaseResponce->getOperation());
     }
@@ -81,7 +83,7 @@ class FirebaseResponceTest extends PHPUnit_Framework_TestCase
     {
         /* not empty */
         $this->assertNotEmpty($this->firebaseResponce->getStatus());
-        
+
         /* type int */
         $this->assertInternalType('int', $this->firebaseResponce->getStatus());
     }
@@ -98,7 +100,7 @@ class FirebaseResponceTest extends PHPUnit_Framework_TestCase
         $this->firebaseResponce->setFirebaseData(null);
         $this->firebaseResponce->setOperation(null);
         $this->firebaseResponce->setStatus(null);
-        
+
         // This function throwed new Exception if something not went well
         $this->firebaseResponce->validateResponce();
     }
